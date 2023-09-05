74c60525ee36d4e2ec9473899c24df822227deab
maolivero89 <maico.olivero26@gmail.com>
2023-09-05 20:05:12 -0300

    ejemplo

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Mundo React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+      <h1>Tareas Pendientes de Hedy Lamarr</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      <ul>
+        <li>Inventar nuevo semáforo</li>
+        <li>Ensayar la escena de la película</li>
+        <li>Mejorar la tecnología del espectro</li>
+      </ul>
+
+
     </div>
   );
 }
